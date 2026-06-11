3f0243635221dfb2a62b0051ee438e2862849a88
dsakariya769-del <dsakariya769@gmail.com>
2026-06-11 18:59:39 +0530

    Update MainActivity.java

--- MainActivity.java
+++ MainActivity.java
@@ -7,25 +7,32 @@ import android.webkit.WebViewClient;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
+
     private WebView myWebView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // WebView ko layout se connect karna
+        myWebView = findViewById(R.id.webView);
         
-        myWebView = new WebView(this);
+        // WebSettings configure karna taaki Google properly chale
         WebSettings webSettings = myWebView.getSettings();
-        webSettings.setJavaScriptEnabled(true); 
-        webSettings.setDomStorageEnabled(true);  
-        
+        webSettings.setJavaScriptEnabled(true);
+        webSettings.setDomStorageEnabled(true);
+        webSettings.setUseWideViewPort(true);
+        webSettings.setLoadWithOverviewMode(true);
+
+        // Links ko app ke andar hi kholne ke liye
         myWebView.setWebViewClient(new WebViewClient());
-        
-        // Yahan aap apni pasand ka URL dal sakte hain
-        myWebView.loadUrl("https://www.google.com"); 
-        
-        setContentView(myWebView);
+
+        // Jab app khule toh sabse pehle Google load ho
+        myWebView.loadUrl("https://www.google.com");
     }
 
+    // Back button dabane par browser page peeche jaye, app band na ho
     @Override
     public void onBackPressed() {
         if (myWebView.canGoBack()) {
